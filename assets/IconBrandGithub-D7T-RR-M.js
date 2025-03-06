d0885ecef9921145513bb8a20cf5aa4e6a6b1d7a
TheAlan404 <TheAlan404@users.noreply.github.com>
2025-03-06 10:33:52 +0000

    deploy: 7b0f1c231702dab2b363caff339b7d0f2628e4ae

--- assets/IconBrandGithub-D7T-RR-M.js
+++ assets/IconBrandGithub-D7T-RR-M.js
@@ -1,5 +1,5 @@
-import{c as a}from"./createReactComponent-DnPgA1Mo.js";/**
- * @license @tabler/icons-react v3.30.0 - MIT
+import{c as a}from"./createReactComponent-DOmhmC3E.js";/**
+ * @license @tabler/icons-react v3.31.0 - MIT
  *
  * This source code is licensed under the MIT license.
  * See the LICENSE file in the root directory of this source tree.
