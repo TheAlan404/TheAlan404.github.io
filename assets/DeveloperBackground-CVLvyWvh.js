b9445fe4cda82b0d39b8b40bf7e73f27439afaf6
TheAlan404 <TheAlan404@users.noreply.github.com>
2025-03-28 16:14:35 +0000

    deploy: 504c45b7aa46bd39ce3c1d863fbf3ea4ff8f3976

---
+++ assets/DeveloperBackground-CVLvyWvh.js
@@ -0,0 +1,31 @@
+import{j as a}from"./jsx-runtime-BjG_zV1W.js";import{P as o}from"./Paper-jWcTi5x0.js";import{u as d}from"./useTranslation-CAIOSfob.js";import{S as h,G as c}from"./Stack-CzsZRHDP.js";import{j as r,I as p,k as i,m as l,n as v,p as g,q as k,r as m,t as u,v as B,w as M,x as y,T as I}from"./IconBrandVite-CnW_yiL2.js";import{T as x}from"./Text-Ccrzb7dV.js";import{B as j}from"./polymorphic-factory-YA6n3l9L.js";/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var b=r("outline","brand-auth0","IconBrandAuth0",[["path",{d:"M12 14.5l-5.5 3.5l2 -6l-4.5 -4h6l2 -5l2 5h6l-4.5 4l2 6z",key:"svg-0"}],["path",{d:"M20.507 8.872l-2.01 -5.872h-12.994l-2.009 5.872c-1.242 3.593 -.135 7.094 3.249 9.407l5.257 3.721l5.257 -3.721c3.385 -2.313 4.49 -5.814 3.25 -9.407z",key:"svg-1"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var f=r("outline","brand-docker","IconBrandDocker",[["path",{d:"M22 12.54c-1.804 -.345 -2.701 -1.08 -3.523 -2.94c-.487 .696 -1.102 1.568 -.92 2.4c.028 .238 -.32 1 -.557 1h-14c0 5.208 3.164 7 6.196 7c4.124 .022 7.828 -1.376 9.854 -5c1.146 -.101 2.296 -1.505 2.95 -2.46z",key:"svg-0"}],["path",{d:"M5 10h3v3h-3z",key:"svg-1"}],["path",{d:"M8 10h3v3h-3z",key:"svg-2"}],["path",{d:"M11 10h3v3h-3z",key:"svg-3"}],["path",{d:"M8 7h3v3h-3z",key:"svg-4"}],["path",{d:"M11 7h3v3h-3z",key:"svg-5"}],["path",{d:"M11 4h3v3h-3z",key:"svg-6"}],["path",{d:"M4.571 18c1.5 0 2.047 -.074 2.958 -.78",key:"svg-7"}],["path",{d:"M10 16l0 .01",key:"svg-8"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var z=r("outline","brand-firefox","IconBrandFirefox",[["path",{d:"M4.028 7.82a9 9 0 1 0 12.823 -3.4c-1.636 -1.02 -3.064 -1.02 -4.851 -1.02h-1.647",key:"svg-0"}],["path",{d:"M4.914 9.485c-1.756 -1.569 -.805 -5.38 .109 -6.17c.086 .896 .585 1.208 1.111 1.685c.88 -.275 1.313 -.282 1.867 0c.82 -.91 1.694 -2.354 2.628 -2.093c-1.082 1.741 -.07 3.733 1.371 4.173c-.17 .975 -1.484 1.913 -2.76 2.686c-1.296 .938 -.722 1.85 0 2.234c.949 .506 3.611 -1 4.545 .354c-1.698 .102 -1.536 3.107 -3.983 2.727c2.523 .957 4.345 .462 5.458 -.34c1.965 -1.52 2.879 -3.542 2.879 -5.557c-.014 -1.398 .194 -2.695 -1.26 -4.75",key:"svg-1"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var S=r("outline","brand-git","IconBrandGit",[["path",{d:"M16 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-0"}],["path",{d:"M12 8m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-1"}],["path",{d:"M12 16m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-2"}],["path",{d:"M12 15v-6",key:"svg-3"}],["path",{d:"M15 11l-2 -2",key:"svg-4"}],["path",{d:"M11 7l-1.9 -1.9",key:"svg-5"}],["path",{d:"M13.446 2.6l7.955 7.954a2.045 2.045 0 0 1 0 2.892l-7.955 7.955a2.045 2.045 0 0 1 -2.892 0l-7.955 -7.955a2.045 2.045 0 0 1 0 -2.892l7.955 -7.955a2.045 2.045 0 0 1 2.892 0z",key:"svg-6"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var G=r("outline","brand-github","IconBrandGithub",[["path",{d:"M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5",key:"svg-0"}]]);/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var T=r("outline","brand-vscode","IconBrandVscode",[["path",{d:"M16 3v18l4 -2.5v-13z",key:"svg-0"}],["path",{d:"M9.165 13.903l-4.165 3.597l-2 -1l4.333 -4.5m1.735 -1.802l6.932 -7.198v5l-4.795 4.141",key:"svg-1"}],["path",{d:"M16 16.5l-11 -10l-2 1l13 13.5",key:"svg-2"}]]);const J=({hoverable:s,className:n,...t})=>a.jsx(o,{p:"xs",className:`frost ${s?"hoverable":""} ${n}`,withBorder:!0,...t}),w=()=>{const[s]=d();return a.jsxs(h,{gap:0,children:[a.jsx(e,{data:[[p,"TypeScript"],[i,"React"],[l,".NET 5-8"],[v,"HTML"],[g,"CSS"],[k,"JavaScript"],[m,"Rust"]]}),a.jsxs(c,{align:"center",gap:8,children:[a.jsx(e,{data:[[u,"Vite"],[B,"MongoDB"],[M,"Prisma ORM"],[b,"Auth0"],[y,"NodeJS"]]}),a.jsx(x,{inline:!0,span:!0,h:24,c:"dimmed",children:"•"}),a.jsx(e,{data:[[z,"Firefox"],[T,"vscode"],[S,"Git"],[G,"Github"],[f,"Docker"]]})]})]})},e=({data:s})=>a.jsx(c,{justify:"center",gap:0,children:s.map(([n,t],R)=>a.jsx(I,{label:t,disabled:!t,children:a.jsx(j,{children:a.jsx(n,{})})}))});export{w as D,G as I,J as S};
